2e05f1933eb90fd858aefb17bd96cd1a9ef8c2be
Divit Reddy Allu <lkuntla@googlemail.com>
2026-01-01 21:55:36 +0100

    Add files via upload

---
+++ L2_ACP_birthday.py
@@ -0,0 +1,11 @@
+ravi_birthday="1.4.2013"
+arjun_birthday="2.10.2013"
+rahul_birthday="21.12.2013"
+abhi_birthday="18.6.2012"
+aarush_birthday="30.8.2013"
+
+print("Ravi birtday is on", ravi_birthday)
+print("arjun birthday is on",arjun_birthday )
+print("rahul birthday is on",rahul_birthday)
+print("abhi birthday is on",abhi_birthday)
+print("aarush birthday is on",aarush_birthday)
