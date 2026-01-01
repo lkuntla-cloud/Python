532c96880ef916801897f5f3b6917e2bc5919f1f
Divit Reddy Allu <lkuntla@googlemail.com>
2026-01-01 17:59:02 +0100

    ACP_Birthday_Dates

---
+++ birthday_dates_ACP.py
@@ -0,0 +1,17 @@
+Ravi= ("1.4.2013")
+Arjun=("2.10.2013")
+Rahul=("21.12.2013")
+Abhi=("18.6.2012")
+Aarush=("30.8.2013")
+name= input("Hello Raju! What is the name of the person whose birthday you want to know?")
+if name  == ("Ravi"):
+    print(Ravi)
+elif name ==("Arjun"):
+    print(Arjun)
+elif name ==("Rahul"):
+    print(Rahul)
+elif name ==("Abhi"):
+    print(Abhi)
+elif name ==("Aarush"):
+    print(Aarush)
+else:print("this name is not in the list")
