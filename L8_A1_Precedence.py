569ef704c6e777f20845172f6e029b48df9965f3
Divit Reddy Allu <lkuntla@googlemail.com>
2026-01-22 15:46:40 +0100

    Lessom 8 Actvities

---
+++ L8_A1_Precedence.py
@@ -0,0 +1,14 @@
+a=10
+b=16
+c=25
+d=2
+e=5
+pre=(a+b)/d*e
+print(pre)
+
+name="Andy"
+age=14
+if name=="Andy" or name =="Tom" and age == 13:
+    print("The name is Andy and he is 14")
+else:
+    print("Nothing is correct")
